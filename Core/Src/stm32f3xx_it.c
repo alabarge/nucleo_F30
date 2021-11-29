550c2f0b5ebf9ac8901a0eb0de7adb1d394abad2
Aaron LaBarge <aaron@omniware.us>
2021-11-29 08:01:10 -0800

    Fixed hard fault handler.

--- Core/Src/stm32f3xx_it.c
+++ Core/Src/stm32f3xx_it.c
@@ -81,7 +81,7 @@ void HardFault_Handler_C(uint32_t *hardfault_args, uint32_t lr_value) {
     lr = hardfault_args[ 5 ];
     pc = hardfault_args[ 6 ];
     psr = hardfault_args[ 7 ];
-    
+
     xlprint("\n\n");
     xlprint("[HardFault] Exception\n\n");
     xlprint("- Stack Frame:\n\n");
@@ -107,7 +107,7 @@ void HardFault_Handler_C(uint32_t *hardfault_args, uint32_t lr_value) {
     xlprint("\n\n");
 
     while(1);
-    
+
 }
 
 /* USER CODE END 0 */
@@ -145,7 +145,17 @@ void NMI_Handler(void)
 void HardFault_Handler(void)
 {
   /* USER CODE BEGIN HardFault_IRQn 0 */
-
+    __asm volatile
+    (
+        " tst lr, #4                                                \n"
+        " ite eq                                                    \n"
+        " mrseq r0, msp                                             \n"
+        " mrsne r0, psp                                             \n"
+        " ldr r1, [r0, #24]                                         \n"
+        " ldr r2, handler2_address_const                            \n"
+        " bx r2                                                     \n"
+        " handler2_address_const: .word HardFault_Handler_C         \n"
+    );
   /* USER CODE END HardFault_IRQn 0 */
   while (1)
   {
@@ -160,17 +170,6 @@ void HardFault_Handler(void)
 void MemManage_Handler(void)
 {
   /* USER CODE BEGIN MemoryManagement_IRQn 0 */
-    __asm volatile
-    (
-        " tst lr, #4                                                \n"
-        " ite eq                                                    \n"
-        " mrseq r0, msp                                             \n"
-        " mrsne r0, psp                                             \n"
-        " ldr r1, [r0, #24]                                         \n"
-        " ldr r2, handler2_address_const                            \n"
-        " bx r2                                                     \n"
-        " handler2_address_const: .word HardFault_Handler_C         \n"
-    );
   /* USER CODE END MemoryManagement_IRQn 0 */
   while (1)
   {
@@ -275,7 +274,7 @@ void SysTick_Handler(void)
 void TIM2_IRQHandler(void)
 {
   /* USER CODE BEGIN TIM2_IRQn 0 */
-  
+
   /* USER CODE END TIM2_IRQn 0 */
   HAL_TIM_IRQHandler(&htim2);
   /* USER CODE BEGIN TIM2_IRQn 1 */
@@ -292,7 +291,7 @@ void USART1_IRQHandler(void)
 
   uart_isr();
   return;
-   
+
   /* USER CODE END USART1_IRQn 0 */
   HAL_UART_IRQHandler(&huart1);
   /* USER CODE BEGIN USART1_IRQn 1 */
@@ -324,7 +323,7 @@ void TIM6_DAC_IRQHandler(void)
   /* USER CODE BEGIN TIM6_DAC_IRQn 0 */
 
      timer();
-   
+
   /* USER CODE END TIM6_DAC_IRQn 0 */
   HAL_TIM_IRQHandler(&htim6);
   /* USER CODE BEGIN TIM6_DAC_IRQn 1 */
